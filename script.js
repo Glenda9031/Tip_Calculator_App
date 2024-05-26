66db4235adab2134c0a5d057841b08d07206442e
Glenda Joyce Reed <100007624+Glenda9031@users.noreply.github.com>
2024-05-26 17:01:15 -0500

    Revised index.html, style.css and script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const buttons = document.querySelectorAll('button');
+const buttonSelected = document.getElementsByClassName('active');
+const reset = document.querySelector("#main-right #button-reset");
+const inputs = document.querySelectorAll('input');
+const resultTip = document.querySelector('#result-tip');
+const resultTotal = document.querySelector('#result-total');
+const bill = document.querySelector('main #main-left #bill .input');
+const numberOfPeople = document.querySelector('main #main-left-bottom #number-people .input');
+const errorMessage = document.querySelector('.if-zero-number');
+const customTip = document.querySelector('.custom');
+
